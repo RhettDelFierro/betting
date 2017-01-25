31491144e9fd3cef2d18c967940ea47785047dfd
Rhett Del Fierro <thinquetank@gmail.com>
2017-01-24 19:37:58 -0800

    parsing for both x an why in the main do block are working

--- app/Main.hs
+++ app/Main.hs
@@ -27,10 +27,6 @@ teamURLS :: [String]
 teamURLS = fmap makeURL teams
     where makeURL = (\(_,team_id,_) -> "http://stats.nba.com/stats/teamgamelog/?Season=2015-16&SeasonType=Regular%20Season&TeamID=" ++ show team_id)
 
-
-jsonURL :: String
-jsonURL = "http://stats.nba.com/stats/teamgamelog/?Season=2015-16&SeasonType=Regular%20Season&TeamID=1610612747"
-
 getJSON :: IO [B.ByteString]
 getJSON = mapM simpleHttp teamURLS
 
@@ -49,8 +45,11 @@ main = do
   ds <- getJSON
   d <- return (eitherDecode <$> ds) :: IO [Either String (Maybe FullPage)]
 --  --d <- (eitherDecode <$> ds) :: IO (Either String ([Maybe FullPage])) --decode lifts over IO to hit the B.ByteString contained in the IO.
-  putStrLn . show $ d
-  putStrLn . show $ fmap checkFullPage $ d
+  --putStrLn . show $ d
+  let x = fmap checkFullPage $ d
+      y = fmap (getBoxScores . read) x
+
+  print y
 --  putStrLn . show $ (fmap check d)
 --                        where check = (\case Left err -> err
 --                                             Right ds -> showResult $ ds)
@@ -72,7 +71,8 @@ checkFullPage = (\case Left err -> err
 showResult :: FullPage -> [GameResult]
 showResult = (rowSet . DL.head . resultSets)
 
-getBoxScores :: [GameResult] -> [(GameResult,GameResult)]
+getBoxScores :: [GameResult] -> (GameResult,GameResult)
+getBoxScores (x:y:ys) = (x,y)
 --getBoxScores (x:xs) = (\case { Just b -> (x,b); Nothing -> (x,x) }) $ DL.find (\y -> (game_ID x) == (game_ID y)) xs
 --getBoxScores arr = DL.groupBy ((==) `on` game_ID) $ arr
 --    where comp [x,y]
@@ -82,13 +82,13 @@ getBoxScores :: [GameResult] -> [(GameResult,GameResult)]
 --      where boxscore t1 t2
 --                | (wl t1) == 'W' = (t1,t2)
 --                | otherwise      = (t2,t1)
-getBoxScores []     = []
-getBoxScores (x:xs) =
-    let sameGame = [boxscore a | a <- xs, (game_ID x) == (game_ID a) ]
-    in getBoxScores xs ++ sameGame
-        where boxscore t2
-                 | (wl x) == 'W' = (x,t2)
-                 | otherwise      = (t2,x)
+--getBoxScores []     = []
+--getBoxScores (x:xs) =
+--    let sameGame = [boxscore a | a <- xs, (game_ID x) == (game_ID a) ]
+--    in getBoxScores xs ++ sameGame
+--        where boxscore t2
+--                 | (wl x) == 'W' = (x,t2)
+--                 | otherwise      = (t2,x)
 --gatherResults :: [GameResult] -> GameLogs
 --gatherResults gr = case gr of
 --                     Just x ->
